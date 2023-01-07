e0814ca87ea4d9d8fb0d6c0f70ff8f162abd6906
HuyTrongTran <tranhuytrong.1821@gmail.com>
2023-01-07 11:59:20 +0700

    first commit

---
+++ test_a.py
@@ -0,0 +1,10 @@
+a = "Hello VietNamese"
+string = a.split()
+item = 0
+print(len(string))
+# print(max)
+# for i in len(string):
+#     if len(string[i] > item):
+#         item = string[i]
+# print(item)
+    
